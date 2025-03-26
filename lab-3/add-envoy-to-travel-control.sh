debb1c38f7beadd3c49ab3cb3177a8e05b87c58d
Ortwin Schneider <ortwin.schneider@googlemail.com>
2025-03-26 17:58:14 +0100

    m3 - update control-vm name

--- lab-3/add-envoy-to-travel-control.sh
+++ lab-3/add-envoy-to-travel-control.sh
@@ -6,5 +6,5 @@ echo "Update VirtualMachine CRs to include in the mesh by injecting Istio and Ja
 echo "---------------------------------------------------------------------------------"
 echo
 
-oc patch VirtualMachine/control-vm --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-control
-oc delete pods -l vm.kubevirt.io/name=control-vm -n travel-control
+oc patch VirtualMachine/travel-control-vm-pool-0 --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-control
+oc delete pods -l vm.kubevirt.io/name=travel-control-vm-pool-0 -n travel-control
