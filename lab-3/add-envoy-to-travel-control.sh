fd38208dcade5bf8aa0b2058ccc0e8b323ec23a7
skoussou <stelios.kousouris>
2025-05-09 15:17:23 +0100

    lab-3 read dynamically the name of the vm pool

--- lab-3/add-envoy-to-travel-control.sh
+++ lab-3/add-envoy-to-travel-control.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
-
-
 echo 
 echo "Update VirtualMachine CRs to include in the mesh by injecting Istio sidecars to components"
 echo "------------------------------------------------------------------------------------------"
 echo
 
-oc patch VirtualMachine/travel-control-vm-pool-0 --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-control
-oc delete pods -l vm.kubevirt.io/name=travel-control-vm-pool-0 -n travel-control
+VM_POOL_NAME=$(oc get VirtualMachine -o jsonpath='{.items[0].metadata.name}')
+echo $VM_POOL_NAME
+
+oc patch VirtualMachine/$VM_POOL_NAME --type=merge -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject": "true"}}}}}' -n travel-control
+oc delete pods -l vm.kubevirt.io/name=$VM_POOL_NAME -n travel-control
 
 echo
 echo
